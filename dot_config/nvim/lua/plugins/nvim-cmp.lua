eabf800af98eae14ab2f6bfb6ebf91b67e51b939
SickMcNugget <sickmcnugget00@pm.me>
2024-01-15 14:18:15 +0800

    Cleaned up completion keys, fixed spelling error

--- dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -2,11 +2,16 @@
 -- See `:help cmp`
 
 return {
+	-- autocomplete
 	"hrsh7th/nvim-cmp",
 	dependencies = {
+		-- Snippet engine
 		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip",
+
+		-- LSP autocomplete
 		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-path",
 		"rafamadriz/friendly-snippets",
 	},
 	config = function()
@@ -18,45 +23,27 @@ return {
 		cmp.setup({
 			snippet = {
 				expand = function(args)
-					luasnip.lsp_extend(args.body)
+					luasnip.lsp_expand(args.body)
 				end,
+			},
+			completion = {
+				completeopt = 'menu,menuone,noinsert',
 			},
 			mapping = cmp.mapping.preset.insert({
 				["<C-n>"] = cmp.mapping.select_next_item(),
 				["<C-p>"] = cmp.mapping.select_prev_item(),
-				["<C-d>"] = cmp.mapping.scroll_docs(-4),
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete({}),
-				-- ["<C-e>"] = cmp.mapping {
-				--     i = cmp.mapping.abort(),
-				--     c = cmp.mapping.close(),
-				-- },
 				["<CR>"] = cmp.mapping.confirm({
 					behavior = cmp.ConfirmBehavior.Replace,
 					select = false,
 				}),
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.locally_jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
 			}),
 			sources = {
 				{ name = "nvim_lsp" },
 				{ name = "luasnip" },
+				{ name = "path" },
 			},
 		})
 	end,
