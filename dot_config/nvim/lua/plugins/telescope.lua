bb7f7908f6a8effc0959dcc2283b494c19733d34
SickMcNugget <sickmcnugget00@pm.me>
2024-01-16 23:36:43 +0800

    Added live grep to telescope :)

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -30,7 +30,7 @@ return {
 		pcall(telescope.load_extension, "fzf")
 
 		-- See `:help telescope.builtin`
-		-- vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+		vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 		vim.keymap.set(
 			"n",
 			"<leader><space>",
@@ -51,5 +51,6 @@ return {
 		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch with [G]rep" })
 	end,
 }
