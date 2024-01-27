ac693f9ef248ee7e243dee4bec40e2e4ee01bf0f
SickMcNugget <sickmcnugget00@pm.me>
2024-01-27 21:25:00 +0800

    Remove vim-tmux-navigator
    I think a better solution would be to learn how to use tmux-sessionizer

--- dot_config/nvim/lua/plugins/vim-tmux-navigator.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"christoomey/vim-tmux-navigator",
-	config = function()
-		vim.keymap.set("n", "<C-h>", vim.cmd.TmuxNavigateLeft)
-		vim.keymap.set("n", "<C-j>", vim.cmd.TmuxNavigateUp)
-		vim.keymap.set("n", "<C-k>", vim.cmd.TmuxNavigateDown)
-		vim.keymap.set("n", "<C-l>", vim.cmd.TmuxNavigateRight)
-	end,
-}
