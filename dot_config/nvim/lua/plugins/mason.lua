162728106fd05a3b849d8bfaf00d402cb97d0073
SickMcNugget <sickmcnugget00@pm.me>
2024-10-20 12:16:57 +0800

    Removed latex from neovim - prefer overleaf CE

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -1,43 +1,42 @@
 return {
-    "williamboman/mason.nvim",
-    cmd = { "Mason" },
-    dependencies = "williamboman/mason-lspconfig.nvim",
-    config = function()
-	local mason = require("mason")
-	local mason_lspconfig = require("mason-lspconfig")
-	local lspconfig = require("lspconfig")
+	"williamboman/mason.nvim",
+	cmd = { "Mason" },
+	dependencies = "williamboman/mason-lspconfig.nvim",
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local lspconfig = require("lspconfig")
 
-	local auto_install = {
-	    "pylsp",
-	    "lua_ls",
-	    "texlab",
-	}
-
-	mason.setup({
-	    ui = {
-		check_outdated_packages_on_open = false,
-		border = "none",
-	    },
-	})
-
-	mason_lspconfig.setup({
-	    ensure_installed = auto_install,
-	})
-
-	mason_lspconfig.setup_handlers({
-	    function(server_name)
-		local opts = {
-		    on_attach = require("plugins.lsp.handlers").on_attach,
-		    capabilities = require("plugins.lsp.handlers").capabilities,
+		local auto_install = {
+			"pylsp",
+			"lua_ls",
 		}
 
-		local require_ok, server = pcall(require, "plugins.lsp.settings." .. server_name)
-		if require_ok then
-		    opts = vim.tbl_deep_extend("force", server, opts)
-		end
+		mason.setup({
+			ui = {
+				check_outdated_packages_on_open = false,
+				border = "none",
+			},
+		})
 
-		lspconfig[server_name].setup(opts)
-	    end,
-	})
-    end,
+		mason_lspconfig.setup({
+			ensure_installed = auto_install,
+		})
+
+		mason_lspconfig.setup_handlers({
+			function(server_name)
+				local opts = {
+					on_attach = require("plugins.lsp.handlers").on_attach,
+					capabilities = require("plugins.lsp.handlers").capabilities,
+				}
+
+				local require_ok, server = pcall(require, "plugins.lsp.settings." .. server_name)
+				if require_ok then
+					opts = vim.tbl_deep_extend("force", server, opts)
+				end
+
+				lspconfig[server_name].setup(opts)
+			end,
+		})
+	end,
 }
