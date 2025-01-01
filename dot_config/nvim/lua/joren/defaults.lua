94669ac41c5cd2809a1ccdab27f0950bc2a164de
SickMcNugget <sickmcnugget00@pm.me>
2025-01-01 15:52:24 +0800

    Added slint filetype

--- dot_config/nvim/lua/joren/defaults.lua
+++ dot_config/nvim/lua/joren/defaults.lua
@@ -60,6 +60,13 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	pattern = "*",
 })
 
+-- Add .slint filetype
+vim.filetype.add({
+	extension = {
+		slint = 'slint'
+	}
+})
+
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
