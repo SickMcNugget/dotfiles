45a6227e5133592c55e5e2fa2e151405ef63dcc8
SickMcNugget <sickmcnugget00@pm.me>
2026-06-02 22:30:20 +0800

    Update and fix telescope.nvim

--- home/dot_config/nvim/lua/plugins/telescope.lua
+++ home/dot_config/nvim/lua/plugins/telescope.lua
@@ -3,34 +3,20 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	event = "VimEnter",
-	tag = "0.1.8",
+	tag = "v0.2.1",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		{
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "make",
-			cond = function()
-				return vim.fn.executable("make") == 1
-			end,
-		},
-		{ "nvim-telescope/telescope-ui-select.nvim" },
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
 	config = function()
 		local telescope = require("telescope")
 		local themes = require("telescope.themes")
 		local builtin = require("telescope.builtin")
 		local utils = require("telescope.utils")
-		telescope.setup({
-			extensions = {
-				["ui-select"] = {
-					themes.get_dropdown(),
-				},
-			},
-		})
+		telescope.setup()
 
 		-- Enable telescope fzf native, if installed
-		pcall(telescope.load_extension, "fzf")
-		pcall(telescope.load_extension, "ui-select")
+		require("telescope").load_extension("fzf")
 
 		vim.keymap.set("n", "<leader>/", function()
 			builtin.current_buffer_fuzzy_find(themes.get_dropdown({
@@ -46,20 +32,20 @@ return {
 		vim.keymap.set("n", "<leader>sn", function()
 			builtin.find_files({ cwd = vim.fn.stdpath("config"), hidden = true, no_ignore = true })
 		end, { desc = "[S]earch [N]eovim files" })
-		-- if vim.fn.executable("chezmoi") == 1 then
-		-- 	local ret = vim.system({ "chezmoi", "source-path" }, { text = true }):wait()
-		-- 	local chezmoi_dir = string.gsub(ret.stdout, "\n", "")
-		-- 	vim.keymap.set("n", "<leader>sc", function()
-		-- 		builtin.find_files({ cwd = chezmoi_dir, hidden = true })
-		-- 	end, { desc = "[S]earch [C]hezmoi files" })
-		-- end
+		if vim.fn.executable("chezmoi") == 1 then
+			local ret = vim.system({ "chezmoi", "source-path" }, { text = true }):wait()
+			local chezmoi_dir = string.gsub(ret.stdout, "\n", "")
+			vim.keymap.set("n", "<leader>sc", function()
+				builtin.find_files({ cwd = chezmoi_dir, hidden = true })
+			end, { desc = "[S]earch [C]hezmoi files" })
+		end
 		vim.keymap.set("n", "<leader>sm", builtin.man_pages, { desc = "[S]earch [M]anpages" })
 		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 		vim.keymap.set("n", "<leader>s/", function()
 			builtin.live_grep({ cwd = utils.buffer_dir() })
-		end, { desc = "[s/] Search with grep " })
+		end, { desc = "[S/] Search with grep " })
 		vim.keymap.set("n", "<leader>.", builtin.oldfiles, { desc = "[.] Search recent files" })
 	end,
 }
